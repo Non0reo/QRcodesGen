b2eca16fd3c6f16c6e104984b6cd874902c82220
No NOréo <70480609+Non0reo@users.noreply.github.com>
2022-02-11 01:13:23 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let number = 60;
 let prefix = "Box";
+let borderColor = "#dcdcdc";
 
 async function getData(prefix, i) {
     const response = await fetch("https://chart.apis.google.com/chart?cht=qr&chs=85x85&chl=" + prefix + i);
@@ -10,7 +11,7 @@ async function getData(prefix, i) {
     let text = document.createElement('p')
     //img.style = 'position:fixed;top:10px;left:10px;width:100px';
     div.id = "qrContainer" + i;
-    div.style = 'text-align:center;width: 90px; height 90px;border: 1px solid #000;float: left;';
+    div.style = 'text-align:center;width: 90px; height 90px;border: 1px solid ' + borderColor + ';float: left;';
     text.textContent = prefix + i;
     text.style = 'font-family: Arial, Helvetica, sans-serif;text-align:center;font-size:20px;display: inline;';
     document.body.append(div);
@@ -30,6 +31,7 @@ function generate(){
 
   number = document.getElementById("numberBox").value;
   prefix = document.getElementById("prefixBox").value;
+  borderColor = document.getElementById("colorBox").value;
   for (let i = 0; i < number; i++) {
     getData(prefix, i + 1); 
   }
